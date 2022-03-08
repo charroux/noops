13e521db35925586e302571d7a6db8f0c5662f9a
AxelTdev <47759642+AxelTdev@users.noreply.github.com>
2022-03-08 16:54:10 +0100

    Update GenerateConfigurationFilesApp.java

--- generateConfigurationFiles/src/generateConfigurationFiles/GenerateConfigurationFilesApp.java
+++ generateConfigurationFiles/src/generateConfigurationFiles/GenerateConfigurationFilesApp.java
@@ -13,104 +13,104 @@ public class GenerateConfigurationFilesApp {
 
 	public static void main(String[] args) {
 
-		//CrÈation d'un fichier de configuration secret.yml
-		//ParamËtres: nom du secret et le mot de passe 
+		//Cr√©ation d'un fichier de configuration secret.yml
+		//Param√®tres: nom du secret et le mot de passe 
 		Secret secret=new Secret("mysql-secret", "test1234");
 		secret.createSecretFile();
 		
 		
-		//CrÈation d'un fichier de configuration storage.yaml
-		//ParamËtres: nom du volume, nom du volume claim, capacitÈ et rÈpertoire de stockage
+		//Cr√©ation d'un fichier de configuration storage.yaml
+		//Param√®tres: nom du volume, nom du volume claim, capacit√© et r√©pertoire de stockage
 		Storage storage=new Storage("mysql-pv-volume", "mysql-pv-claim", "20Gi", "/mnt/data");
 		storage.createStorageFile();
 		
 		
-		//CrÈation d'un fichier de configuration deploymentDB.yaml => Pour les bases de donnÈes
-		//ParamËtres: nom du dÈploiement,nom de image,instance de la classe Secret, rÈpertoire de stockage, port, instance de la classe Storage
+		//Cr√©ation d'un fichier de configuration deploymentDB.yaml => Pour les bases de donn√©es
+		//Param√®tres: nom du d√©ploiement,nom de image,instance de la classe Secret, r√©pertoire de stockage, port, instance de la classe Storage
 		DeploymentDB depDB=new DeploymentDB("mysql", "mysql:5.7", secret, "/var/lib/mysql", 3306, storage);
 		depDB.createDeploymentDBFile();
 		
 		
-		//CrÈation d'un fichier de configuration deploymentApp.yaml => Pour les applications avec un replicas par dÈfaut: 1
-		//ParamËtres: nom du dÈploiement et nom de image
+		//Cr√©ation d'un fichier de configuration deploymentApp.yaml => Pour les applications avec un replicas par d√©faut: 1
+		//Param√®tres: nom du d√©ploiement et nom de image
 		DeploymentApp depApp=new DeploymentApp("my-service", "efrei/my-service:latest");
 		depApp.createDeploymentAppFile();
 		
 		
-		//CrÈation d'un fichier de configuration deploymentApp.yaml => Pour les applications avec un nombre de replicas donnÈ
-		//ParamËtres: nom du dÈploiement, nom de image et le nombre de replicas
+		//Cr√©ation d'un fichier de configuration deploymentApp.yaml => Pour les applications avec un nombre de replicas donn√©
+		//Param√®tres: nom du d√©ploiement, nom de image et le nombre de replicas
 		DeploymentApp depApp2=new DeploymentApp("my-service", "efrei/my-service:latest",2);
 		depApp2.createDeploymentAppFile();
 		
 		
-		//CrÈation d'un fichier de configuration serviceClusterIp.yaml pour les applications
-		//ParamËtres: instance de la classe DeploymentApp et port
+		//Cr√©ation d'un fichier de configuration serviceClusterIp.yaml pour les applications
+		//Param√®tres: instance de la classe DeploymentApp et port
 		ServiceClusterIp serviceClusterIpApp=new ServiceClusterIp(depApp, 3306);
 		serviceClusterIpApp.createServiceClusterIpFileApp();
 		
 		
-		//CrÈation d'un fichier de configuration serviceClusterIp.yaml pour les bases de donnÈes
-		//ParamËtres: instance de la classe DeploymentDB et port
+		//Cr√©ation d'un fichier de configuration serviceClusterIp.yaml pour les bases de donn√©es
+		//Param√®tres: instance de la classe DeploymentDB et port
 		ServiceClusterIp serviceClusterIpDB=new ServiceClusterIp(depDB, 3306);
-		serviceClusterIpDB.createServiceClusterIpFileApp();
+		serviceClusterIpDB.createServiceClusterIpFileDB();
 		
 		
-		//CrÈation d'un fichier de configuration serviceNodePort.yaml avec un Nodeport par dÈfaut: 31281 pour les applications
-		//ParamËtres: instance de la classe DeploymentApp et port
+		//Cr√©ation d'un fichier de configuration serviceNodePort.yaml avec un Nodeport par d√©faut: 31281 pour les applications
+		//Param√®tres: instance de la classe DeploymentApp et port
 		ServiceNodePort serviceNodePortApp=new ServiceNodePort(depApp2, 3306);
 		serviceNodePortApp.createServiceNodePortFileApp();
 		
 		
-		//CrÈation d'un fichier de configuration serviceNodePort.yaml avec un NodePort donnÈ pour les applications
-		//ParamËtres: instance de la classe DeploymentApp, port et nodePort
+		//Cr√©ation d'un fichier de configuration serviceNodePort.yaml avec un NodePort donn√© pour les applications
+		//Param√®tres: instance de la classe DeploymentApp, port et nodePort
 		ServiceNodePort serviceNodePortApp2=new ServiceNodePort(depApp2, 3306, 31283);
 		serviceNodePortApp2.createServiceNodePortFileApp();
 	
 
-		//CrÈation d'un fichier de configuration serviceNodePort.yaml avec un Nodeport par dÈfaut: 31281 pour les DB
-		//ParamËtres: instance de la classe DeploymentDB et port
+		//Cr√©ation d'un fichier de configuration serviceNodePort.yaml avec un Nodeport par d√©faut: 31290 pour les DB
+		//Param√®tres: instance de la classe DeploymentDB et port
 		ServiceNodePort serviceNodePortDB=new ServiceNodePort(depDB, 3306);
 		serviceNodePortDB.createServiceNodePortFileDB();
 		
 		
-		//CrÈation d'un fichier de configuration serviceNodePort.yaml avec un NodePort donnÈ pour les DB
-		//ParamËtres: instance de la classe DeploymentDB, port et nodePort
+		//Cr√©ation d'un fichier de configuration serviceNodePort.yaml avec un NodePort donn√© pour les DB
+		//Param√®tres: instance de la classe DeploymentDB, port et nodePort
 		ServiceNodePort serviceNodePortDB2=new ServiceNodePort(depDB, 3306, 31283);
 		serviceNodePortDB2.createServiceNodePortFileDB();
 		
 		
-		//CrÈation d'un fichier de configuration serviceLoadBalancer.yaml avec un Nodeport par dÈfaut: 31285 pour les applications
-		//ParamËtres: instance de la classe DeploymentApp et port
+		//Cr√©ation d'un fichier de configuration serviceLoadBalancer.yaml avec un Nodeport par d√©faut: 31285 pour les applications
+		//Param√®tres: instance de la classe DeploymentApp et port
 		ServiceLoadBalancer serviceLoadBalancerApp=new ServiceLoadBalancer(depApp2, 8080);
 		serviceLoadBalancerApp.createServiceLoadBalancerFile();
 		
 		
-		//CrÈation d'un fichier de configuration serviceClusterNodePort.yaml avec un NodePort donnÈ pour les applications
-		//ParamËtres: instance de la classe DeploymentApp, port et nodePort
+		//Cr√©ation d'un fichier de configuration serviceClusterNodePort.yaml avec un NodePort donn√© pour les applications
+		//Param√®tres: instance de la classe DeploymentApp, port et nodePort
 		ServiceLoadBalancer serviceLoadBalancerApp2=new ServiceLoadBalancer(depApp2, 8080, 31289);
 		serviceLoadBalancerApp2.createServiceLoadBalancerFile();
 		
 		
-		//CrÈation d'un fichier  Dockerfile avec une version de java par dÈfaut: 8
-		//ParamËtres: nom du jar et port
+		//Cr√©ation d'un fichier  Dockerfile avec une version de java par d√©faut: 8
+		//Param√®tres: nom du jar et port
 		Docker dockerfile1=new Docker("MyService-0.0.1-SNAPSHOT.jar", 8080);
 		dockerfile1.createDockerFile();
 		
 		
-		//CrÈation d'un fichier de configuration Dockerfile avec une version de java donnÈe
-		//ParamËtres: nom du jar, port et une version de java
+		//Cr√©ation d'un fichier de configuration Dockerfile avec une version de java donn√©e
+		//Param√®tres: nom du jar, port et une version de java
 		Docker dockerfile2=new Docker("MyService-0.0.1-SNAPSHOT.jar", 8080, 11);
 		dockerfile2.createDockerFile();
 		
 		
-		//CrÈation d'un fichier  Dockerfile avec une version de java et un port par dÈfaut: 8 et 8080
-		//ParamËtres: nom du jar
+		//Cr√©ation d'un fichier  Dockerfile avec une version de java et un port par d√©faut: 8 et 8080
+		//Param√®tres: nom du jar
 		Docker dockerfile3=new Docker("MyService-0.0.1-SNAPSHOT.jar");
 		dockerfile3.createDockerFile();
 		
 		
-		//CrÈation d'un fichier de configuration Dockerfile avec une version de java donnÈe et un port par dÈfaut 8080
-		//ParamËtres: Une version de java et nom du jar
+		//Cr√©ation d'un fichier de configuration Dockerfile avec une version de java donn√©e et un port par d√©faut 8080
+		//Param√®tres: Une version de java et nom du jar
 		Docker dockerfile4=new Docker(11,"MyService-0.0.1-SNAPSHOT.jar");
 		dockerfile4.createDockerFile();
 		
